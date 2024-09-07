fc1dea888064b732ea8cd22e2bf2a88f8cb84cdc
nina1982-ops <ya.nina.82@yandex.ru>
2024-09-07 16:02:30 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -2,9 +2,9 @@ def custom_write(file_name, strings):
     file_name = 'text.txt'
     strings_positions = {}
     file = open(file_name, 'w', encoding='utf-8')
-    for i, string in enumerate(strings):
+    for i, string in enumerate(strings, 1):
         file.write(string + '\n')
-        strings_positions[(i + 1, file.tell() - len(string) - 1)] = string
+        strings_positions[(i, file.tell())] = string
     file.close()
     return strings_positions
 
